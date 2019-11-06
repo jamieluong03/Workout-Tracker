71ce83c2d6a7a86a4acebbdfed5733b8fc2d474d
Jamie Luong <jamieluong03@gmail.com>
2019-11-06 12:37:24 -0800

    updated html routes

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+// get "api/exercise"
+
+// get "api/exercise/:id"
+
+// get "api/workouts"
+
+// post "api/workouts"
+
+// put "api/exercise/:id"
+
 
 module.exports = router;
