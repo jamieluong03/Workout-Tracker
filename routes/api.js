c4bb19d1b6fea25dd200cb7cd384ff531820b7c0
Jamie Luong <jamieluong03@gmail.com>
2019-11-07 01:48:51 -0800

    updated some get and post in workout.js

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,36 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-// get "api/exercise"
+// get "/api/workouts"
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+});
 
-// get "api/exercise/:id"
+// get "/api/workouts/range"
 
-// get "api/workouts"
 
-// post "api/workouts"
+// post "/api/workouts"
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
 
-// put "api/exercise/:id"
+// put "/api/workouts/:id"
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.find({})
+})
+
 
 
 module.exports = router;
--- routes/view.js
+++ routes/view.js
@@ -9,7 +9,7 @@ router.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.get("/workouts", function(req, res) {
+router.get("/stats", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
