9c97a84c2f43e54aac369b26e9a21fca83295275
Jamie Luong <jamieluong03@gmail.com>
2019-11-08 23:08:50 -0800

    changed uri

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Workout-Tracker
+
+This app allows users to track and record their daily workout routines. The user has a choice to either continue a workout or start a new workout. 
+
+## Links
+* https://fierce-oasis-05187.herokuapp.com
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds141218.mlab.com:41218/heroku_8rmwjtw9";
 // old route "mongodb://localhost/workout"
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
